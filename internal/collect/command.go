96d2ae42c6333bd04fffafa90513e5635c14bb75
Alex-Pgr <258459042+Alex-Pgr@users.noreply.github.com>
2026-09-13 09:57:40 +0300

    Make collector command timeout testable

--- internal/collect/command.go
+++ internal/collect/command.go
@@ -9,13 +9,21 @@ import (
 const externalCommandTimeout = 8 * time.Second
 
 func commandOutput(name string, args ...string) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), externalCommandTimeout)
+	return commandOutputTimeout(externalCommandTimeout, name, args...)
+}
+
+func commandOutputTimeout(timeout time.Duration, name string, args ...string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	return exec.CommandContext(ctx, name, args...).Output()
 }
 
 func commandCombinedOutput(name string, args ...string) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), externalCommandTimeout)
+	return commandCombinedOutputTimeout(externalCommandTimeout, name, args...)
+}
+
+func commandCombinedOutputTimeout(timeout time.Duration, name string, args ...string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	return exec.CommandContext(ctx, name, args...).CombinedOutput()
 }
