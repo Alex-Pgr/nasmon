2a4ad964bdef879e8c27890f97367f02a975a256
Alex-Pgr <258459042+Alex-Pgr@users.noreply.github.com>
2026-09-15 11:51:31 +0300

    Update Unicode layout test import paths

--- internal/tui/unicode_layout_test.go
+++ internal/tui/unicode_layout_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 	"time"
 
-	"nasmon/internal/app"
-	"nasmon/internal/model"
+	"github.com/Alex-Pgr/nas_monitoring/internal/app"
+	"github.com/Alex-Pgr/nas_monitoring/internal/model"
 )
 
 func TestUnicodeUserTextStaysWithinLandscapeWidth(t *testing.T) {
